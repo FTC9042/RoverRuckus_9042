607f0f0d7b871f5798c4e9577a34eab2015b3b02
Anikait Singh <singh.anikait@gmail.com>
2018-10-03 18:42:00 -0400

    added potentiometer testing

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/StringPotTest.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode.tests;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.AnalogInput;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.util.Potentiometer;
+
+@Autonomous(name = "StringPotTest")
+public class StringPotTest extends OpMode {
+    private Potentiometer potentiometer;
+
+    @Override
+    public void init() {
+        potentiometer = new Potentiometer(hardwareMap);
+
+    }
+
+    @Override
+    public void loop() {
+        telemetry.addData("Voltage: ", potentiometer.getVoltage());
+        telemetry.addData("Max Voltage: ", potentiometer.getMaxVoltage());
+        telemetry.update();
+    }
+}
