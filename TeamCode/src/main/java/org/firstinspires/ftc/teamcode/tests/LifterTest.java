c23667938871f397c45a517e8734a5207f718c78
Anikait Singh <singh.anikait@gmail.com>
2018-11-08 13:44:26 -0500

    brunswick

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/LifterTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/LifterTest.java
@@ -3,26 +3,41 @@ package org.firstinspires.ftc.teamcode.tests;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 @Autonomous(name = "Lifter Test")
 public class LifterTest  extends OpMode {
     DcMotor extend;
     DcMotor left_lift, right_lift;
+    private DcMotor intake;
+
+    double power = 0;
 
     @Override
     public void init() {
         extend = hardwareMap.dcMotor.get("0");
         left_lift = hardwareMap.dcMotor.get("1");
         right_lift = hardwareMap.dcMotor.get("2");
+        intake = hardwareMap.dcMotor.get("3");
 
+        extend.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         left_lift.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         right_lift.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        right_lift.setDirection(DcMotorSimple.Direction.REVERSE);
     }
 
     @Override
     public void loop() {
-        extend.setPower(gamepad1.left_stick_y);
+        extend.setPower(-gamepad1.left_stick_y);
         left_lift.setPower(gamepad1.right_stick_y);
         right_lift.setPower(gamepad1.right_stick_y);
+        intake.setPower(power);
+        if(gamepad1.left_bumper){
+            power = 1;
+        }
+
+        if(gamepad1.right_bumper){
+            power = -1;
+        }
     }
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vision/GoldAlign.java
@@ -0,0 +1,68 @@
+package org.firstinspires.ftc.teamcode.vision;
+
+import com.disnodeteam.dogecv.CameraViewDisplay;
+import com.disnodeteam.dogecv.DogeCV;
+import com.disnodeteam.dogecv.detectors.roverrukus.GoldAlignDetector;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.util.DriveTrain;
+
+@Autonomous(name = "Align Gold")
+public class GoldAlign extends LinearOpMode {
+    // Detector object
+    private GoldAlignDetector detector;
+    DriveTrain driveTrain;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        // Set up detector
+        detector = new GoldAlignDetector();
+        detector.init(hardwareMap.appContext, CameraViewDisplay.getInstance());
+        detector.useDefaults();
+
+        detector.alignSize = 100;
+        detector.alignPosOffset = 0;
+        detector.downscale = 0.4;
+
+        detector.areaScoringMethod = DogeCV.AreaScoringMethod.MAX_AREA;
+        detector.maxAreaScorer.weight = 0.005;
+
+        detector.ratioScorer.weight = 5;
+        detector.ratioScorer.perfectRatio = 1.0;
+
+        detector.enable();
+
+        driveTrain = new DriveTrain(hardwareMap);
+
+        driveTrain.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        waitForStart();
+        driveTrain.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        driveTrain.setPower(-0.3,0.3);
+        ElapsedTime t = new ElapsedTime();
+        while(t.seconds()<3 && opModeIsActive()){
+            telemetry.addData("Turning", "Left");
+            telemetry.update();
+        }
+        driveTrain.setPower(0);
+
+        t.reset();
+        driveTrain.setPower(0.1,-0.1);
+        while(opModeIsActive() && t.seconds()<10){
+            if(detector.getAligned()){
+                break;
+            }
+        }
+        driveTrain.setPower(0);
+
+        t.reset();
+        driveTrain.setPower(0.7);
+        while (opModeIsActive() && t.seconds()<3){
+            telemetry.addData("Hitting", "Mineral");
+        }
+        driveTrain.setPower(0);
+    }
+}
